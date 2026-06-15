ee8d7b94e93f82bced0952949bf66a674c978677
Anirudh s <anirudhs@Mac.attlocal.net>
2026-06-15 16:22:27 -0400

    Configuration file m3

--- app/config/config.py
+++ app/config/config.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    model_config= SettingsConfigDict(env_file =".env")
+    model_config = SettingsConfigDict(env_file=".env")
+
     anthropic_api_key: str = ""
     github_token: str = ""
-    model_name:str = "claude-haiku-4-5"
+    model_name: str = "claude-haiku-4-5"
+
 
 settings = Settings()
